6e2d7b1ad9b76bbc4e962fa714931157caf6e642
Betül Akgül <76072632+betulAkgull@users.noreply.github.com>
2024-08-21 12:38:13 +0300

    listing update and listing delete

--- listings/views.py
+++ listings/views.py
@@ -34,3 +34,32 @@ def listing_create(request):
         "form":form
     }
     return render(request, "listing_create.html",context)
+
+
+def listing_update(request,pk):
+    listing = Listing.objects.get(id = pk)
+    form = ListingForm(instance=listing)
+    if request.method == 'POST':
+        form = ListingForm(request.POST, instance= listing)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Listing saved successfully!")
+            return redirect('/', pk=listing.pk)
+        else:
+            messages.error(request, "Something got wrong, try again.")
+            print(form.errors)
+    context = {
+        "form":form
+    }
+    return render(request, "listing_update.html",context)
+
+
+def listing_delete(request,pk):
+    listing = Listing.objects.get(id = pk)
+    if request.method == 'POST':
+        listing.delete()
+        messages.success(request, "Listing deleted successfully!")
+        return redirect('/')
+    else:
+        return redirect('listing.html', pk=pk) 
+    
--- real_estate/urls.py
+++ real_estate/urls.py
@@ -2,11 +2,13 @@
 from django.contrib import admin
 from django.urls import path
 
-from listings.views import listing_list, listings_retrieve, listing_create
+from listings.views import listing_list, listings_retrieve, listing_create, listing_update, listing_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listing_list, name='listing_list'),
     path('listings/<pk>/', listings_retrieve, name='listing_retrieve'),
-    path('add-listing/', listing_create, name='listing_create')
+    path('add-listing/', listing_create, name='listing_create'),
+    path('update-listing/<pk>/edit/',listing_update,name='listing_update'),
+    path('delete-listing/<pk>/delete/',listing_delete,name='listing_delete')
 ]
